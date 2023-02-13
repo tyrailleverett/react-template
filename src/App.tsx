851315bf135b5698fbaa7d9bc13177fecad44d1e
Tyrail Leverett <tyrailleverett@gmail.com>
2023-02-13 10:01:37 -0600

    feat: added playwright

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { Helmet } from "react-helmet-async";
+
 function App() {
     return (
         <>
+            <Helmet>
+                <title>Home</title>
+            </Helmet>
             <h1 className="text-3xl font-bold underline">Hello world!</h1>
         </>
     );
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { MantineProvider } from '@mantine/core';
+import { MantineProvider } from "@mantine/core";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-<App />
-    </MantineProvider>
-    
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <MantineProvider withGlobalStyles withNormalizeCSS>
+            <HelmetProvider>
+                <App />
+            </HelmetProvider>
+        </MantineProvider>
+    </React.StrictMode>
+);
